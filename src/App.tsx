779064d520dfe3e54060bce322922b0438f348b5
zhangyanhua <939593210@qq.com>
2022-04-07 19:47:32 +0800

    firstcommit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Header } from "./component/Header";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
